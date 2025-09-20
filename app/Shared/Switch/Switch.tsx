bbb55c466fdbb0835cf2221ad3c8fc1ee13a5951
312351 <igdpwwijaya@gmail.com>
2025-09-20 14:55:08 +0800

    Wawewe

--- app/Shared/Switch/Switch.tsx
+++ app/Shared/Switch/Switch.tsx
@@ -1,35 +1,35 @@
-"use client";
-import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+  "use client";
+  import { motion } from "framer-motion";
+  import { useEffect, useState } from "react";
 
-interface AnimateSwitchProps {
-  Texts: string[];
-  Class: String[]
-}
+  interface AnimateSwitchProps {
+    Texts: string[];
+    Class?: string[];
+  }
 
-const Switch = ({ Texts,Class }: AnimateSwitchProps) => {
-  const [index, setIndex] = useState(0);
+  const Switch = ({ Texts,Class }: AnimateSwitchProps) => {
+    const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % Texts.length);
-    }, 5000);
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setIndex((prevIndex) => (prevIndex + 1) % Texts.length);
+      }, 5000);
 
-    return () => clearInterval(interval);
-  }, [Texts.length]);
+      return () => clearInterval(interval);
+    }, [Texts.length]);
 
-  return (
-      <motion.span
-        key={index}
-        initial={{ opacity: 0.5, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-        transition={{ duration: 1 }}
-        className={`w-auto ${Class}`}
-      >
-        {Texts[index]}
-      </motion.span>
-  );
-};
+    return (
+        <motion.span
+          key={index}
+          initial={{ opacity: 0.5, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 1 }}
+          className={`w-auto ${Class}`}
+        >
+          {Texts[index]}
+        </motion.span>
+    );
+  };
 
-export default Switch;
+  export default Switch;
